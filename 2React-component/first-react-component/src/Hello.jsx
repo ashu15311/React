ed4bfb25dcf98bfb4829f77bcaa1cb8b906d59b7
Ashutosh <dmashutosh01@gmail.com>
2024-03-04 21:09:44 +0530

    Ch 2 React Component

---
+++ 2React-component/first-react-component/src/Hello.jsx
@@ -0,0 +1,13 @@
+function Hello(){
+
+    var myName="Ashu";
+    let number=69;
+    let fullName=() =>{
+        return 'Ashutosh';
+    }
+    return <p>
+        Message no:{number}/Hello This is the Future Speaking .I am {fullName()}
+    </p>
+}
+
+export default Hello;
---
+++ 2React-component/first-react-component/src/Random.jsx
@@ -0,0 +1,6 @@
+function Random(){
+    let number=Math.random() * 100;
+    return <h1 style={{'backgroundColor':'yellow'}}>Random Number is:{Math.round(number)}</h1>
+}
+
+export default Random;
